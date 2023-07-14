3ffed4fe4d474e1f4d8be87c31955b5e84278714
amanuil <81981466+amanuil@users.noreply.github.com>
2023-07-14 16:20:49 +0300

    updated

---
+++ src/constant/index.js
@@ -0,0 +1,9 @@
+export const NavLinks =[
+    {href:'/about', key:'about', text:'We Are'},
+    {href:'/event', key:'event', text:'Events'},
+    {href:'/v_tube', key:'v_tube', text:'V_Tube'},
+    {href:'/shop', key:'shop', text:'Shop'},
+]
+export const FtrLinks =[
+    {},
+]
